13119c9f4c45e56f4f4686c7ee04bcb580d12b36
ghoghoghog <98801250+ghoghoghog@users.noreply.github.com>
2023-08-24 11:29:13 +0900

    werqwerqwer

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 
 public class TestCollision : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+    private void Update()
     {
-        Debug.Log("collision");
-            
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y,
+                Camera.main.nearClipPlane));
+            Vector3 dir = mousePos - Camera.main.transform.position;
+            dir = dir.normalized;
+
+            Debug.DrawRay(Camera.main.transform.position, dir * 100, Color.red, 1);
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100))
+            {
+                Debug.Log($"Raycast Camera @{hit.collider.gameObject.name}");
+            }
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("trigger");
-    }
 }
