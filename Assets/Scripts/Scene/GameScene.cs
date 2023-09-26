ce76eee397b13bfeee104a45d964ac4600121982
ghoghoghog <98801250+ghoghoghog@users.noreply.github.com>
2023-09-26 13:54:32 +0900

    asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf

--- Assets/Scripts/Scene/GameScene.cs
+++ Assets/Scripts/Scene/GameScene.cs
@@ -6,13 +6,30 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+
+    class CoroutineTest : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;        }
+    }
     protected override void Init()
     {
         base.Init();
         SceneType = Define.Scene.Game;
         Manager.UI.ShowSceneUI<UI_Inven>();
 
-        for (int i = 0; i < 3; i++)
+        CoroutineTest test = new CoroutineTest();
+        foreach (System.Object t in test)
+        {
+            int value = (int)t;
+            Debug.Log(value);
+        }
+
+        for (int i = 0; i < 10; i++)
         {
             Manager.Resource.Instantiate("unitychan");
             Manager.Resource.Instantiate("Tank");
